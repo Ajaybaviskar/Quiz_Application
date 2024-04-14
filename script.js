9e6542026d253e918c18eddb69734ba09eeaf995
Ajay Baviskar <ajaybaviskar830@gmail.com>
2024-04-14 12:39:30 +0530

    Add Some Sample Question for Testing

---
+++ script.js
@@ -0,0 +1,35 @@
+const questions = [
+    {
+        questions:"Which is largest animal in the world?",
+        answers:[
+            {text:"Shark",correct:false},
+            {text:"Blue whale",correct:true},
+            {text:"Elephant",correct:false},
+            {text:"Giraffe",correct:false},
+
+
+        ]
+    },
+    {
+        questions:"Which is largest desert in the world?",
+        answers:[
+            {text:"Kalahari",correct:false},
+            {text:"gobi",correct:true},
+            {text:"Sahara",correct:false},
+            {text:"Antrctica",correct:true},
+
+
+        ]
+    },
+    {
+        questions:"Which is the smallest continent in the world?",
+        answers:[
+            {text:"Asia",correct:false},
+            {text:"Australia",correct:true},
+            {text:"Arctic",correct:false},
+            {text:"Africa",correct:false},
+
+
+        ]
+    }
+]
