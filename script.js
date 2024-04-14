2b51f2a8782ae5d6730a3daf49886bb698429af9
Ajay Baviskar <ajaybaviskar830@gmail.com>
2024-04-14 16:27:45 +0530

    Add Logic for Clicking Question And Answer

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
+// Question Section Start  
 const questions = [
     {
-        questions:"Which is largest animal in the world?",
+        question:"Which is largest animal in the world?",
         answers:[
             {text:"Shark",correct:false},
             {text:"Blue whale",correct:true},
             {text:"Elephant",correct:false},
             {text:"Giraffe",correct:false},
-
-
         ]
     },
     {
-        questions:"Which is largest desert in the world?",
+        question:"Which is largest desert in the world?",
         answers:[
             {text:"Kalahari",correct:false},
             {text:"gobi",correct:true},
             {text:"Sahara",correct:false},
             {text:"Antrctica",correct:true},
-
-
         ]
     },
     {
-        questions:"Which is the smallest continent in the world?",
+        question:"Which is the smallest continent in the world?",
         answers:[
             {text:"Asia",correct:false},
             {text:"Australia",correct:true},
             {text:"Arctic",correct:false},
             {text:"Africa",correct:false},
-
-
+        ]
+    },
+    {
+        question:"Which is the smallest continent in the world?",
+        answers:[
+            {text:"Asia",correct:false},
+            {text:"Australia",correct:true},
+            {text:"Arctic",correct:false},
+            {text:"Africa",correct:false},
         ]
     }
-]
+];  
+// Question End 
+const questionElement=document.getElementById("question");
+const answerButton=document.getElementById("answer-buttons");
+const nextButton=document.getElementById("next-btn");
+
+// add score login  
+let currentQuestionIndex=0;
+let score=0; 
+
+function startQuiz(){
+    currentQuestionIndex=0;
+    score=0; 
+    nextButton.innerHTML="Next"; 
+    showQuestion(); 
+}
+
+function showQuestion(){
+    let currentQuestion=questions[currentQuestionIndex];
+    let questionNo=currentQuestionIndex+1;
+    questionElement.innerHTML=questionNo+". "+currentQuestion.question; 
+
+    // target answer buttons
+    currentQuestion.answers.forEach(answer=>{
+        const button=document.createElement("button");
+        button.innerHTML=answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+
+    })
+
+    
+} 
+
+startQuiz();
